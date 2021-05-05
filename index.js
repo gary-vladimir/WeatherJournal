8740c28e938f9f0c2147af76c5091594cfb5ac9e
Gary Nuñez <yragi.nunez@gmail.com>
2021-05-05 14:26:37 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const submit = document.getElementById('submit');
+const description = document.getElementById('description');
+const temperature = document.getElementById('temperature');
+const weatherIcon = document.getElementById('weatherIcon');
+const inputCity = document.getElementById('inputCity');
+const placeImg = document.getElementById('placeImg');
+
+submit.addEventListener('click', main);
+
+function main() {
+    console.log('click');
+
+    let description;
+    let lat;
+    let lng;
+    let temperature;
+    let icon;
+    let image;
+    let country;
+    fetchGeonames().then((data) => {
+        lat = data.geonames[0].lat;
+        lng = data.geonames[0].lng;
+        country = data.geonames[0].countryName;
+        fetchWeather(lat, lng).then((data) => {
+            description = data.data[0].weather.description;
+            temperature = data.data[0].temp;
+            icon = data.data[0].weather.icon;
+            fetchPixabay(country).then((data) => {
+                image = data.hits[0].largeImageURL;
+                render(description, temperature, icon, image);
+            });
+        });
+    });
+}
+
+function fetchGeonames() {
+    return fetch(
+        `http://api.geonames.org/searchJSON?q=${inputCity.value}&maxRows=1&username=` // add your username
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => console.log(error));
+}
+
+function fetchWeather(latitude, longitude) {
+    return fetch(
+        `https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=` // add your weatherbit api
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => console.log(error));
+}
+
+function fetchPixabay(country) {
+    return fetch(
+        `https://pixabay.com/api/?key=&image_type=photo&orientation=horizontal&per_page=3&pretty=true&q=${country}` // add your Pixabay key
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => console.log(error));
+}
+
+function render(des, temp, icon, image) {
+    description.innerHTML = des;
+    temperature.innerHTML = `${temp}°C`;
+    weatherIcon.src = `/iconosWeather/${icon}.svg`;
+    placeImg.style.backgroundImage = `url(${image})`;
+}
